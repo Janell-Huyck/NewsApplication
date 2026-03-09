386aa3c9c6458329e44a939253d442379ca114a1
Janell Huyck <huyckjl@ucmail.uc.edu>
2026-03-08 21:14:00 -0400

    UX changes to list and detail pages

--- NewsApplication/Pages/NewsDetailPage.xaml.cs
+++ NewsApplication/Pages/NewsDetailPage.xaml.cs
@@ -7,7 +7,6 @@ public partial class NewsDetailPage : ContentPage
 	public NewsDetailPage(Article article)
 	{
 		InitializeComponent();
-		Title = article.Title;
 		DetailImage.Source = article.Image;
 		DetailTitle.Text = article.Title;
 		DetailText.Text = article.Content ?? article.Description ?? string.Empty;
